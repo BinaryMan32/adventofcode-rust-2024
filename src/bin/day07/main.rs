7a742336b4136f1bb8e34d7ba98cc1b386ce71f8
Fred Fettinger <fettinge@gmail.com>
2024-12-08 01:22:56 -0600

    day07 part 2

--- src/bin/day07/main.rs
+++ src/bin/day07/main.rs
@@ -1,33 +1,65 @@
 use advent_of_code::{create_runner, named, Named, Runner};
 use itertools::Itertools;
+use num::pow;
 use std::str::Lines;
+
+struct Number {
+    value: u64,
+    shift: u64,
+}
+
+impl Number {
+    fn parse(text: &str) -> Option<Self> {
+        let value = text.parse().ok()?;
+        let shift = pow(10, text.len());
+        Some(Self{value, shift})
+    }
+}
 
 struct Equation {
     test_value: u64,
-    numbers: Vec<u64>,
+    numbers: Vec<Number>,
 }
 
 impl Equation {
     fn parse(line: &str) -> Option<Self> {
         let (test_value, numbers) = line.split_once(": ")?;
         let test_value = test_value.parse().ok()?;
-        let numbers = numbers.split_ascii_whitespace().flat_map(|n| n.parse()).collect_vec();
+        let numbers = numbers.split_ascii_whitespace().flat_map(Number::parse).collect_vec();
         Some(Self{test_value, numbers})
     }
 
     fn can_solve_add_mul(&self) -> bool {
-        self.can_solve_add_mul_aux(&self.numbers[1..], self.numbers[0])
+        self.can_solve_add_mul_aux(&self.numbers[1..], self.numbers[0].value)
     }
 
-    fn can_solve_add_mul_aux(&self, remaining: &[u64], result: u64) -> bool {
+    fn can_solve_add_mul_aux(&self, remaining: &[Number], result: u64) -> bool {
         if remaining.is_empty() {
             result == self.test_value
         } else if result > self.test_value {
             false
         } else {
-            let first = remaining[0];
+            let first = remaining[0].value;
             let remaining = &remaining[1..];
             self.can_solve_add_mul_aux(remaining, result + first) || self.can_solve_add_mul_aux(remaining, result * first)
+        }
+    }
+
+    fn can_solve_add_mul_cat(&self) -> bool {
+        self.can_solve_add_mul_cat_aux(&self.numbers[1..], self.numbers[0].value)
+    }
+
+    fn can_solve_add_mul_cat_aux(&self, remaining: &[Number], result: u64) -> bool {
+        if remaining.is_empty() {
+            result == self.test_value
+        } else if result > self.test_value {
+            false
+        } else {
+            let first = &remaining[0];
+            let remaining = &remaining[1..];
+            self.can_solve_add_mul_cat_aux(remaining, result + first.value) ||
+                self.can_solve_add_mul_cat_aux(remaining, result * first.value) ||
+                self.can_solve_add_mul_cat_aux(remaining, result * first.shift + first.value)
         }
     }
 }
@@ -42,7 +74,12 @@ fn part1(input: Lines) -> String {
 }
 
 fn part2(input: Lines) -> String {
-    input.take(0).count().to_string()
+    input
+        .flat_map(Equation::parse)
+        .filter(Equation::can_solve_add_mul_cat)
+        .map(|eq| eq.test_value)
+        .sum::<u64>()
+        .to_string()
 }
 
 fn main() {
@@ -61,6 +98,6 @@ mod tests {
     fn example() {
         let input = include_str!("example.txt");
         verify!(part1, input, "3749");
-        verify!(part2, input, "0");
+        verify!(part2, input, "11387");
     }
 }
