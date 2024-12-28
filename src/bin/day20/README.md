90d01f6ee44849704c26a1da75f929ae890603a8
Fred Fettinger <fettinge@gmail.com>
2024-12-28 16:06:04 -0600

    day20 part 1

--- src/bin/day20/README.md
+++ src/bin/day20/README.md
@@ -26,15 +26,15 @@ They hand you a map of the racetrack (your puzzle input). For example:
 ###############
 ```
 
-The map consists of track (.) - including the start (S) and end (E) positions (both of which also count as track) - and walls (#).
+The map consists of track (`.`) - including the start (`S`) and end (`E`) positions (both of which also count as track) - and walls (`#`).
 
-When a program runs through the racetrack, it starts at the start position. Then, it is allowed to move up, down, left, or right; each such move takes 1 picosecond. The goal is to reach the end position as quickly as possible. In this example racetrack, the fastest time is 84 picoseconds.
+When a program runs through the racetrack, it starts at the start position. Then, it is allowed to move up, down, left, or right; each such move takes `1` picosecond. The goal is to reach the end position as quickly as possible. In this example racetrack, the fastest time is `84` picoseconds.
 
 Because there is only a single path from the start to the end and the programs all go the same speed, the races used to be pretty boring. To make things more interesting, they introduced a new rule to the races: programs are allowed to cheat.
 
-The rules for cheating are very strict. Exactly once during a race, a program may disable collision for up to 2 picoseconds. This allows the program to pass through walls as if they were regular track. At the end of the cheat, the program must be back on normal track again; otherwise, it will receive a segmentation fault and get disqualified.
+The rules for cheating are very strict. Exactly once during a race, a program may disable collision for up to `2` picoseconds. This allows the program to pass through walls as if they were regular track. At the end of the cheat, the program must be back on normal track again; otherwise, it will receive a segmentation fault and get disqualified.
 
-So, a program could complete the course in 72 picoseconds (saving 12 picoseconds) by cheating for the two moves marked 1 and 2:
+So, a program could complete the course in `72` picoseconds (saving `12` picoseconds) by cheating for the two moves marked `1` and `2`:
 
 ```
 ###############
@@ -54,7 +54,7 @@ So, a program could complete the course in 72 picoseconds (saving 12 picoseconds
 ###############
 ```
 
-Or, a program could complete the course in 64 picoseconds (saving 20 picoseconds) by cheating for the two moves marked 1 and 2:
+Or, a program could complete the course in `64` picoseconds (saving `20` picoseconds) by cheating for the two moves marked `1` and `2`:
 
 ```
 ###############
@@ -74,7 +74,7 @@ Or, a program could complete the course in 64 picoseconds (saving 20 picoseconds
 ###############
 ```
 
-This cheat saves 38 picoseconds:
+This cheat saves `38` picoseconds:
 
 ```
 ###############
@@ -94,7 +94,7 @@ This cheat saves 38 picoseconds:
 ###############
 ```
 
-This cheat saves 64 picoseconds and takes the program directly to the end:
+This cheat saves `64` picoseconds and takes the program directly to the end:
 
 ```
 ###############
@@ -130,4 +130,69 @@ In this example, the total number of cheats (grouped by the amount of time they 
 - There is one cheat that saves 40 picoseconds.
 - There is one cheat that saves 64 picoseconds.
 
-You aren't sure what the conditions of the racetrack will be like, so to give yourself as many options as possible, you'll need a list of the best cheats. How many cheats would save you at least 100 picoseconds?
+You aren't sure what the conditions of the racetrack will be like, so to give yourself as many options as possible, you'll need a list of the best cheats. How many cheats would save you at least `100` picoseconds?
+
+## Part 2
+
+The programs seem perplexed by your list of cheats. Apparently, the two-picosecond cheating rule was deprecated several milliseconds ago! The latest version of the cheating rule permits a single cheat that instead lasts at most `20` picoseconds.
+
+Now, in addition to all the cheats that were possible in just two picoseconds, many more cheats are possible. This six-picosecond cheat saves `76` picoseconds:
+
+```
+###############
+#...#...#.....#
+#.#.#.#.#.###.#
+#S#...#.#.#...#
+#1#####.#.#.###
+#2#####.#.#...#
+#3#####.#.###.#
+#456.E#...#...#
+###.#######.###
+#...###...#...#
+#.#####.#.###.#
+#.#...#.#.#...#
+#.#.#.#.#.#.###
+#...#...#...###
+###############
+```
+
+Because this cheat has the same start and end positions as the one above, it's the same cheat, even though the path taken during the cheat is different:
+
+```
+###############
+#...#...#.....#
+#.#.#.#.#.###.#
+#S12..#.#.#...#
+###3###.#.#.###
+###4###.#.#...#
+###5###.#.###.#
+###6.E#...#...#
+###.#######.###
+#...###...#...#
+#.#####.#.###.#
+#.#...#.#.#...#
+#.#.#.#.#.#.###
+#...#...#...###
+###############
+```
+
+Cheats don't need to use all `20` picoseconds; cheats can last any amount of time up to and including `20` picoseconds (but can still only end when the program is on normal track). Any cheat time not used is lost; it can't be saved for another cheat later.
+
+You'll still need a list of the best cheats, but now there are even more to choose between. Here are the quantities of cheats in this example that save `50` picoseconds or more:
+
+- There are 32 cheats that save 50 picoseconds.
+- There are 31 cheats that save 52 picoseconds.
+- There are 29 cheats that save 54 picoseconds.
+- There are 39 cheats that save 56 picoseconds.
+- There are 25 cheats that save 58 picoseconds.
+- There are 23 cheats that save 60 picoseconds.
+- There are 20 cheats that save 62 picoseconds.
+- There are 19 cheats that save 64 picoseconds.
+- There are 12 cheats that save 66 picoseconds.
+- There are 14 cheats that save 68 picoseconds.
+- There are 12 cheats that save 70 picoseconds.
+- There are 22 cheats that save 72 picoseconds.
+- There are 4 cheats that save 74 picoseconds.
+- There are 3 cheats that save 76 picoseconds.
+
+Find the best cheats using the updated cheating rules. How many cheats would save you at least `100` picoseconds?
--- src/bin/day20/main.rs
+++ src/bin/day20/main.rs
@@ -1,8 +1,167 @@
 use advent_of_code::{create_runner, named, Named, Runner};
-use std::str::Lines;
+use glam::U8Vec2;
+use itertools::{repeat_n, Itertools};
+use std::{collections::{HashMap, VecDeque}, str::Lines};
+
+type Pos = U8Vec2;
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+enum CellKind {
+    Track,
+    Wall,
+    Border,
+}
+
+struct Racetrack {
+    cells: Vec<Vec<CellKind>>,
+    size: Pos,
+    start: Pos,
+    end: Pos,
+}
+
+impl Racetrack {
+    fn parse(input: Lines) -> Self {
+        let mut start = None;
+        let mut end = None;
+        let mut cells = input
+            .enumerate()
+            .map(|(y, line)| {
+                line.chars()
+                    .enumerate()
+                    .map(|(x, c)| {
+                        match c {
+                            'S' => {
+                                start = Some(Pos{x: x as u8, y: y as u8});
+                                CellKind::Track
+                            },
+                            'E' => {
+                                end = Some(Pos{x: x as u8, y: y as u8});
+                                CellKind::Track
+                            },
+                            '.' => CellKind::Track,
+                            '#' => CellKind::Wall,
+                            u => panic!("unexpected char {u}")                            
+                        }
+                    })
+                    .collect_vec()
+            })
+            .collect_vec();
+        let size = Pos{x: cells[0].len() as u8, y: cells.len() as u8};
+
+        // overwrite all border cells with a different kind to make it easier
+        // to know not to visit them without checking coordinates
+        cells.first_mut().unwrap().fill(CellKind::Border);
+        cells.last_mut().unwrap().fill(CellKind::Border);
+        cells.iter_mut().for_each(|row| {
+            *row.first_mut().unwrap() = CellKind::Border;
+            *row.last_mut().unwrap() = CellKind::Border;
+        });
+
+        Self{cells, size, start: start.expect("start"), end: end.expect("end")}
+    }
+    
+    fn get_cell(&self, pos: Pos) -> CellKind {
+        self.cells[pos.y as usize][pos.x as usize]
+    }
+
+    fn neighbors(&self, pos: Pos) -> [(Pos, CellKind); 4] {
+        [
+            Pos{x: pos.x + 1, y: pos.y},
+            Pos{x: pos.x, y: pos.y - 1},
+            Pos{x: pos.x - 1, y: pos.y},
+            Pos{x: pos.x, y: pos.y + 1},
+        ].map(|neighbor| (neighbor, self.get_cell(neighbor)))
+    }
+ }
+
+struct TimeSolver<'a> {
+    racetrack: &'a Racetrack,
+    times: Vec<Vec<Option<usize>>>,
+}
+
+impl<'a> TimeSolver<'a> {
+    fn new(racetrack: &'a Racetrack) -> Self {
+        let times = repeat_n(
+            repeat_n(None, racetrack.size.x as usize)
+                .collect_vec(),
+            racetrack.size.y as usize
+        )
+        .collect_vec();
+        Self{racetrack, times}
+    }
+
+    fn solve(racetrack: &'a Racetrack, start: Pos) -> Self {
+        let mut solver = Self::new(racetrack);
+        solver.find_shortest_times(start);
+        solver
+    }
+
+    fn get_time(&self, pos: Pos) -> Option<usize> {
+        self.times[pos.y as usize][pos.x as usize]
+    }
+
+    fn replace_if_better(&mut self, pos: Pos, time: usize) -> bool {
+        let existing = &mut self.times[pos.y as usize][pos.x as usize];
+        if existing.as_ref().is_none_or(|old| time < *old) {
+            *existing = Some(time);
+            true
+        } else {
+            false
+        }
+    }
+
+    fn find_shortest_times(&mut self, start: Pos) {
+        let mut traverse = VecDeque::new();
+        traverse.push_back((start, 0));
+        while let Some((pos, time)) = traverse.pop_front() {
+            if self.replace_if_better(pos, time) {
+                for (npos, kind) in self.racetrack.neighbors(pos) {
+                    if kind == CellKind::Track {
+                        traverse.push_back((npos, time + 1));
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn get_cheat_histogram(racetrack: Racetrack) -> HashMap<usize, usize> {
+    let mut histogram = HashMap::new();
+    let from_start = TimeSolver::solve(&racetrack, racetrack.start);
+    let from_end = TimeSolver::solve(&racetrack, racetrack.end);
+    let best_time_without_cheat = from_end.get_time(racetrack.start)
+        .expect("time from start to end");
+    for y in 0..racetrack.size.y {
+        for x in 0..racetrack.size.x {
+            let start_pos = Pos{x, y};
+            if let Some(start_time) = from_start.get_time(start_pos) {
+                for (cheat_start_pos, kind) in racetrack.neighbors(start_pos) {
+                    if kind == CellKind::Wall {
+                        for (cheat_end_pos, _kind) in racetrack.neighbors(cheat_start_pos) {
+                            // a path to the end without cheating implies CellKind::Track
+                            if let Some(end_time) = from_end.get_time(cheat_end_pos) {    
+                                let time_with_cheat = 2 + start_time + end_time;
+                                let saved = best_time_without_cheat.saturating_sub(time_with_cheat);
+                                if saved > 0 {
+                                    *histogram.entry(saved).or_default() += 1;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    histogram
+}
 
 fn part1(input: Lines) -> String {
-    input.take(0).count().to_string()
+    let racetrack = Racetrack::parse(input);
+    get_cheat_histogram(racetrack)
+        .into_iter()
+        .filter_map(|(saved, count)| (saved >= 100).then_some(count))
+        .sum::<usize>()
+        .to_string()
 }
 
 fn part2(input: Lines) -> String {
@@ -22,8 +181,31 @@ mod tests {
     use advent_of_code::verify;
 
     #[test]
+    fn test_get_cheat_histogram() {
+        let racetrack = Racetrack::parse(include_str!("example.txt").lines());
+        assert_eq!(
+            get_cheat_histogram(racetrack),
+            [
+                (2, 14), // There are 14 cheats that save 2 picoseconds.
+                (4, 14), // There are 14 cheats that save 4 picoseconds.
+                (6, 2), // There are 2 cheats that save 6 picoseconds.
+                (8, 4), // There are 4 cheats that save 8 picoseconds.
+                (10, 2), // There are 2 cheats that save 10 picoseconds.
+                (12, 3), // There are 3 cheats that save 12 picoseconds.
+                (20, 1), // There is one cheat that saves 20 picoseconds.
+                (36, 1), // There is one cheat that saves 36 picoseconds.
+                (38, 1), // There is one cheat that saves 38 picoseconds.
+                (40, 1), // There is one cheat that saves 40 picoseconds.
+                (64, 1), // There is one cheat that saves 64 picoseconds.
+            ].into_iter().collect()
+        );
+    }
+
+    #[test]
     fn example() {
         let input = include_str!("example.txt");
+        // One of the rare puzzles where this test is pointless.
+        // The example is small enough that no cheat can save 100 picoseconds.
         verify!(part1, input, "0");
         verify!(part2, input, "0");
     }
